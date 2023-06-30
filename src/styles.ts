af5e8b7ef75287101c9eb100d5ca796027df35c7
Antonio Annes <antonio.vmachado13@gmail.com>
2023-06-30 16:10:17 -0300

    setando cabecalho

---
+++ src/styles.ts
@@ -0,0 +1,29 @@
+import styled, { createGlobalStyle } from 'styled-components'
+
+export const Cores = {
+  branca: '#eee',
+  preta: '#111',
+  cinza: '#333',
+  verde: '#10Ac84'
+}
+
+export const GlobalCss = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    font-family: Roboto, sans-serif;
+    list-style: none;
+  }
+
+  body {
+    background-color: ${Cores.preta};
+    color: ${Cores.branca};
+    padding-top: 40px;
+  }
+`
+export const Container = styled.div`
+  max-width: 1024px;
+  width: 100%;
+  margin: 0 auto;
+`
