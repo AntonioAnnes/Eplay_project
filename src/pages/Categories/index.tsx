aa21420ef9569f3e56c846b1c4f8b57f066e100d
Antonio Annes <antonio.vmachado13@gmail.com>
2023-07-03 11:36:43 -0300

    final modulo

---
+++ src/pages/Categories/index.tsx
@@ -0,0 +1,105 @@
+import ProductsList from '../../components/ProductsList'
+import Game from '../../models/Game'
+
+import resident from '../../assets/images/resident.png'
+import diablo from '../../assets/images/diablo.png'
+import zelda from '../../assets/images/zelda.png'
+import star_wars from '../../assets/images/star_wars.png'
+import streetfighter from '../../assets/images/streetfighter.png'
+
+const promocoes: Game[] = [
+  {
+    id: 1,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror...',
+    title: 'Resident Evil 4',
+    system: 'windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 2,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
+    title: 'Resident Evil 4',
+    system: 'PS5',
+    infos: ['5%', 'R$ 290,00'],
+    image: resident
+  },
+  {
+    id: 3,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
+    title: 'Resident Evil 4',
+    system: 'windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  },
+  {
+    id: 4,
+    category: 'Ação',
+    description:
+      'Resident Evil 4, conhecido no Japão como Biohazard 4, é um jogo eletrônico de survival horror',
+    title: 'Resident Evil 4',
+    system: 'windows',
+    infos: ['10%', 'R$ 250,00'],
+    image: resident
+  }
+]
+
+const emBreve: Game[] = [
+  {
+    id: 5,
+    category: 'RPG',
+    description:
+      'Diablo IV é um RPG de ação em desenvolvimento pela Blizzard Entertainment.',
+    title: 'Diablo 4',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: diablo
+  },
+  {
+    id: 6,
+    category: 'Aventura',
+    description:
+      'Star Wars Jedi: Survivor é um próximo jogo de ação e aventura desenvolvido pela Respawn...',
+    title: 'Star Wars',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: star_wars
+  },
+  {
+    id: 7,
+    category: 'Luta',
+    description:
+      'Street Fighter 6 é um próximo jogo de luta desenvolvido e publicado pela Capcom.',
+    title: 'Street Fighter 6',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: streetfighter
+  },
+  {
+    id: 8,
+    category: 'RPG',
+    description:
+      'Uma aventura épica pela terra e pelos céus de Hyrule aguarda em The Legend of Zelda™...',
+    title: 'The Legend Of Zelda - TOK',
+    system: 'Windows',
+    infos: ['17/05'],
+    image: zelda
+  }
+]
+
+const Categories = () => (
+  <>
+    <ProductsList games={promocoes} title="RPG" background="gray" />
+    <ProductsList games={emBreve} title="Ação" background="black" />
+    <ProductsList games={promocoes} title="Aventura" background="gray" />
+    <ProductsList games={emBreve} title="FPS" background="black" />
+  </>
+)
+
+export default Categories
